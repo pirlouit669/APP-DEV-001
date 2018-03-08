584fc3675384d12b806ba85ffa1954f187b7cdc5
pirlouit669 <pirlouit669@gmail.com>
2018-03-08 09:15:23 +0100

    l

--- js/index.js
+++ js/index.js
@@ -23,6 +23,42 @@ function checkConnection() { // depre ?
 
 
 document.addEventListener('deviceready', readylocal, false);
-function readylocal () {
-      alert('local ready');
+function readylocal () {      
+      $(document).on('pagebeforeshow', '#nointernet', function (e, data) {
+            previous=data.prevPage.attr('id');
+      });
+      
+      
+}
+
+
+function offline() { // ne fonctionne pas si placé dans ready
+      $('.connexion-on').hide();
+      $('.connexion-off').show();
+      $.mobile.changePage($('#nointernet'), 'pop', false, true);
+}
+function online() { // ne fonctionne pas si placé dans ready
+      $('.connexion-off').hide();
+      $('.connexion-on').fadeIn();
+      setTimeout(function() {
+            $.mobile.changePage($('#'+previous));
+      }, 3000);
+}
+
+function videmarchand(){// vide les infos marchands initiales.
+      $("#header-marchand-nom").html('');
+      $("#marchand-details-logo").attr('src', '');
+      $('#details-marchand .ui-content').hide();
+      $('#details-marchand .encours').show();
+      $( ".mobile-fav" ).removeClass( "marchand-favori" );
+      //$(".marchand-container").hide();
+      //$('#details-marchand').prepend('<div class="encours"><i class="fas fa-spinner fa-spin fa-3x"></i></div>');
+      
+}
+function videticket(){// vide les infos ticket initiales.
+      $('#ticket-details').prepend('<div class="encours"><i class="fas fa-spinner fa-spin fa-3x"></i></div>');
+      $('#ticket_nouvelle_reponse').show();
+      $('ticket_nouvelle_reponse_msg').hide();
+      $('#ticket-container').html('');
+      //$('#ticket-container').hide();
 }
